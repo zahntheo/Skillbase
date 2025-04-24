c2bd904232543c2192acda8c5bddaf3008202c73
zahntheo <theozahn2021@gmail.com>
2025-04-24 19:05:44 +0200

    Set up tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",  // Dies stellt sicher, dass Tailwind auch in deinen TSX-Dateien funktioniert
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
